7908bae15f8470b22bf624f93b8aaaaed2e8fa05
chandya <chandya.c99@gmail.com>
2021-09-17 20:24:02 +0800

    course view

---
+++ src/pages/CourseManagement/ViewCourseDetails/index.tsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react'
+import { withRouter, useHistory } from 'react-router-dom';
+import { getCourseByCourseId, toggleEnrollmentActiveStatus } from './../../../apis/Course/CourseApis';
+import { Tag } from "../../../apis/Entities/Tag";
+import { UpdateCourseReq, Course } from "../../../apis/Entities/Course";
+import { Lesson } from "../../../apis/Entities/Lesson";
+import { Multimedia } from "../../../apis/Entities/Multimedia"
+import { getAllTags } from '../../../apis/Tag/TagApis';
+import { Button } from "../../../values/ButtonElements";
+
+
+import { CourseDetailsContainer, HeadingWrapper, CourseDetailsCard, CourseDetailsCardHeader, CourseDetailsContent, CourseDetailsEditContainer } from "./ViewCourseDetailsElements"
+import {
+    useAutocomplete,
+    AutocompleteGetTagProps,
+  } from '@mui/core/AutocompleteUnstyled';
+  import CheckIcon from '@mui/icons-material/Check';
+  import CloseIcon from '@mui/icons-material/Close';
+  import { styled } from '@mui/material/styles';
+
+
+function ViewCourseDetails(props: any) {
+
+    const history = useHistory();
+    const courseId = props.match.params.courseId;
+    const [course, setCourse] = useState<Course>();
+    const [loading, setLoading] = useState<boolean>(true);
+    const [tagLibrary, setTagLibrary] = useState<Tag[]>([]);
+    const [isToggleActiveEnrollmentDialogOpen, setIsToggleActiveEnrollmentDialogOpen] = useState<boolean>(false);
+
+    useEffect(() => {
+        setLoading(true);
+        getCourseByCourseId(courseId).then(receivedCourse => {
+            setCourse(receivedCourse);
+            setLoading(false);
+        });
+    }, [courseId])
+
+    console.log(course);
+
+    return (
+        <CourseDetailsContainer>
+            <HeadingWrapper>Course Details</HeadingWrapper>
+            <CourseDetailsContent>
+            </CourseDetailsContent>
+        </CourseDetailsContainer>
+    )
+}
+
+export default withRouter(ViewCourseDetails);
---
+++ src/pages/CourseManagement/ViewCourseList/ViewCoursesElements.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+import { colours } from "../../../values/Colours"
+import { fontSizes } from "../../../values/FontSizes"
+import { Link as LinkR } from "react-router-dom";
+
+export const CourseListContainer = styled.div`
+    max-width: 75rem;
+    padding: 1rem;
+    margin: calc(50px + 1rem) auto 0;
+    font-family: "Roboto", sans-serif;
+`;
+
+export const HeadingWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    width: 100%;
+    margin-bottom: 1rem;
+    font-size: ${fontSizes.SUBHEADER};
+    color: ${colours.GRAY2};
+    font-weight: bold;
+`;
+
+export const DataGridContainer = styled.div`
+    height: 400px;
+    width: 100%;
+`
+
+export const BtnWrapper = styled.div`
+    margin-top: 10px;
+    width: 150px;
+`
+
+
