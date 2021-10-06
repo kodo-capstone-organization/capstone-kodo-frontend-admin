61802c8e3343ea7b13f182c9e702bde51f73e0f5
chandya <chandya.c99@gmail.com>
2021-10-06 18:27:34 +0800

    styling

--- src/pages/FinanceManagement/PlatformEarnings/PlatformEarningsElements.js
+++ src/pages/FinanceManagement/PlatformEarnings/PlatformEarningsElements.js
@@ -64,9 +64,30 @@ export const Featured = styled.div`
     justify-content: space-between;
 `
 
-export const FeaturedItem = styled.div``
+export const FeaturedItem = styled.div`
+    flex: 1;
+    margin: 0px 20px;
+    padding: 30px;
+    border-radius: 10px;
+    cursor: pointer;
+    -webkit-box-shadow: 0px 0px 15px -10px rgba(0, 0, 0, 0.75);
+    box-shadow: 0px 0px 15px -10px rgba(0, 0, 0, 0.75);
+`
 
-export const FeaturedMoney = styled.span``
+export const FeaturedTitle = styled.div`
+    font-size: 20px;
+`
+
+export const FeaturedMoneyContainer = styled.div`
+    margin: 10px 0px;
+    display: flex;
+    align-items: center;
+`
+
+export const FeaturedMoney = styled.span`
+    font-size: 30px;
+    font-weight: 600;
+`
 
 export const FeaturedMoneyRate = styled.span``
 
